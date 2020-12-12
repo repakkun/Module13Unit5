7c5b706186e54a458e5aec1d3459c6bc8da0657e
rePakkun <cj.ilya@yandex.ru>
2020-12-12 22:35:49 +0300

    Добавьте файлы проекта.

---
+++ Module13Unit5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module13Unit5
+{
+    class Program
+    {
+
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;          
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+            Console.WriteLine("..." + modif);
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.ReadKey();
+        }
+    }
+}
